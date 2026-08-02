c27553916dd25435210e79c581dd8e337fcc2c18
dandy062448 <dandy.062448@gmail.com>
2026-08-01 22:58:25 -0400

    Implement dynamic programming agent

--- hw3/crawler/DP_Agent.py
+++ hw3/crawler/DP_Agent.py
@@ -41,14 +41,39 @@ class DP_Agent(object):
         pass
 
 
+    def compute_value(self, state, action, transition):
+        """
+        Helper function that scores the action.
+        """
+        successor_state, reward = transition(state, action)
+        if successor_state is None:
+            return reward + self.gamma * 0
+        return reward + self.gamma * self.values[successor_state]
+    
+
     def policy_evaluation(self, transition):
         """ Computes all values for current policy by iteration and stores them in self.values.
+            Implemented with in place value iteration.
         Args:
             transition (Callable): Function that returns successor state and reward given a state and action.
         """
-        # TODO
-        pass
 
+        VALUE_CONVERGENCE_THRESHOLD = 1e-6
+
+        convergent = False
+        while not convergent:
+            # Initial assumption
+            convergent = True
+
+            for state in self.policy:
+                action = self.policy[state]
+                new_value = self.compute_value(state, action, transition)
+
+                if new_value - self.values[state] > VALUE_CONVERGENCE_THRESHOLD:
+                    convergent = False
+
+                self.values[state] = new_value
+    
 
     def policy_extraction(self, valid_actions, transition):
         """ Computes all optimal actions using value iteration and stores them in self.policy.
@@ -56,8 +81,13 @@ class DP_Agent(object):
             valid_actions (Callable): Function that returns a list of actions given a state.
             transition (Callable): Function that returns successor state and reward given a state and action.
         """
-        # TODO
-        pass
+
+        for state in self.policy:
+            best_action = max(
+                valid_actions(state), 
+                key=lambda action: self.compute_value(state, action, transition)
+            )
+            self.policy[state] = best_action
 
 
     def policy_iteration(self, valid_actions, transition):
@@ -66,5 +96,13 @@ class DP_Agent(object):
             valid_actions (Callable): Function that returns a list of actions given a state.
             transition (Callable): Function that returns successor state and reward given a state and action.
         """
-        # TODO
-        pass
+        # randomize initial policy.
+        for state in self.policy:
+            self.policy[state] = random.choice(valid_actions(state))
+
+        while True:
+            old_policy = self.policy.copy()
+            self.policy_evaluation(transition)
+            self.policy_extraction(valid_actions, transition)
+            if self.policy == old_policy:
+                break
